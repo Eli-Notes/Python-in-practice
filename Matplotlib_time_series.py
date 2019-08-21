e6cc1916a8b6924f37e4a6f1644f0921f2e6a8f4
Zhang_Qian <1185954839@qq.com>
2019-08-21 10:56:54 +0800

    Update Matplotlib_time_series.py

--- Matplotlib_time_series.py
+++ Matplotlib_time_series.py
@@ -19,8 +19,15 @@ mpl.rcParams['axes.unicode_minus'] = False
 ## single plot with specific figure size - option one
 plt.rcParams['figure.figsize'] = (6, 4)
 fig, ax = plt.subplots(1, 1)
-ax.plot(x, y)  # general plot
-ax.plot_date(datetime_series, value_series, '-', color='tab:orange', label='test') # time series plot
+# general plot
+ax.plot(x, y)
+# time series plot
+ax.plot_date(datetime_series, value_series,
+             '-', # solid line
+             color='tab:orange', # color
+             label='test', # label
+             lw=2 # or linewidth=2
+            ) 
 
 ## single plot with specific figure size - option two
 plt.figure(figsize=(6, 4))
@@ -33,27 +40,29 @@ ax1.plot_date(datetime_series, value_series, '-', color='tab:orange', label='tes
 ax2 = plt.twinx(ax1)
 ax2.plot_date(datetime_series, value_series, '-', color='tab:orange', label='test')
 
-## Multiple subplot
-fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
-ax1.plot...
-ax2.plot...
-ax3.plot...
+## Single figure attributes
 
-## Attributes
-
-### figure size, legend, label, title, grid
-ax.legend(loc=1)
+### figure size, legend, label, title, grid, ylim
+ax.legend(loc=0) # 0 stands for 'best'
+ax.legend(bbox_to_anchor=(1, 1))
 ax.set_xlabel('x')
-ax.set_title(title_name)
+ax.set_title(title_name, fontsize=24)
 ax.grid()
+ax.set_ylim(lowerBound, upperBound)
 
 ### time-tick format
 from matplotlib.dates import DateFormatter
 formatter = DateFormatter('%H:%m')
 ax.xaxis.set_major_formatter(formatter)
 
-### adjustment
+## Multiple subplot
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
+ax1.plot...
+ax2.plot...
+ax3.plot...
 plt.tight_layout()
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+fig.suptitle(title, fontsize=16)
 
 ## save and close
 plt.savefig(file_name, quality=95, dpi=500) # save current figure in default
