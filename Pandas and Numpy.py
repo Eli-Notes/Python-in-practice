2d6ec27220b95cabbe0ba376528b0462efdaef0e
Zhang_Qian <1185954839@qq.com>
2019-08-22 17:22:23 +0800

    Update Pandas and Numpy.py

--- Pandas and Numpy.py
+++ Pandas and Numpy.py
@@ -1,3 +1,3 @@
-# find nan in a dataframe
+# find nan in dataframe, using boolean index
 boolean_index = df['value'].isnull().values.reshape(-1) # -1 stands for 1-d array
 df_output = df.loc[boolean_index] # or df[boolean_index]
