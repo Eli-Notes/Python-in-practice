734e5c00da4eb5c0a9e67e7e0c484fad36b7108e
Zhang_Qian <1185954839@qq.com>
2019-09-23 10:23:59 +0800

    Update Pandas and Numpy.py

--- Pandas and Numpy.py
+++ Pandas and Numpy.py
@@ -2,9 +2,6 @@
 boolean_index = df['value'].isnull().values.reshape(-1) # -1 stands for 1-d array
 df_output = df.loc[boolean_index] # or df[boolean_index]
 
-# deep copy and shallow copy
-# pandas的df和series在传递时默认是浅拷贝；即便是向方程传递参数，也是浅拷贝；所以要及时copy。
-
 # find the index of nan in ndarray
 np.argwhere(np.isnan(data))
 
